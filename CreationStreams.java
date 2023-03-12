54cfa68e68f154ef8c38ef72866f451414661e16
Parvsub <Praveenboda421@gmail.com>
2023-03-12 21:47:18 +0530

    Added: Sort in assending order for given array list

--- CreationStreams.java
+++ CreationStreams.java
@@ -6,9 +6,12 @@ import java.util.stream.Stream;
 
 public class CreationStreams {
     public static void main(String[] args) {
-        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
-        IntSummaryStatistics result = primes.stream()
-                .mapToInt((x) -> x).summaryStatistics();
-        System.out.print(result);
+        List<Integer> list = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+
+        System.out.println("The sorted stream is : ");
+        // displaying the stream with elements
+        // sorted in natural order
+        list.stream().sorted().forEach(System.out::println);
     }
 }
+    
