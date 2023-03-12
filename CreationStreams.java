2074063b853843f407e58552f1b34ce74bcde9fd
Parvsub <Praveenboda421@gmail.com>
2023-03-12 20:05:25 +0530

    Added: Doule the element

--- CreationStreams.java
+++ CreationStreams.java
@@ -3,10 +3,12 @@ package com.Streams;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CreationStreams {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(2,4,6,8,10,4);
-        list.stream().forEach(System.out::println);
+        List<Integer> list = Arrays.asList(2,4,6,8,10,4,5,8,2);
+        List<Integer> squares = list.stream().map(i ->i*i).collect(Collectors.toList());
+        System.out.println("result:" +squares);
     }
 }
