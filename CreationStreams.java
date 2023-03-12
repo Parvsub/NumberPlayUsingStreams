ce1842175a53a61728097e0d55f43aa2f5df9274
Parvsub <Praveenboda421@gmail.com>
2023-03-12 21:24:51 +0530

    Added: First even number is printed

--- CreationStreams.java
+++ CreationStreams.java
@@ -1,13 +1,12 @@
 package com.Streams;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CreationStreams {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(2,4,6,8,10,4,5,3,9,5);
-        list.stream().filter(n->n % 2 ==0).forEach(System.out::println);
+        int[] array = {1, 4, 5, 2, 4, 6, 8, 10, 4, 5, 3, 9, 5};
+        OptionalInt count = Arrays.stream(array).filter(num-> num%2 ==0).findFirst();
+        System.out.println(count);
     }
 }
